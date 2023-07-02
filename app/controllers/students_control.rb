2752c9c3ab5c3d7fe35286ec2c5b59b633144f36
charityj26 <charity.joseph@student.moringaschool.com>
2023-07-02 13:47:48 +0300

    completed assignment

---
+++ app/controllers/students_control.rb
@@ -0,0 +1,13 @@
+class StudentsController < ApplicationController
+
+    def index
+      students = Student.all
+      render json: students
+    end
+  
+    def get_id
+      student_id = Student.find_by(id: params[:id])
+      render json: student_id
+    end
+  
+  end
